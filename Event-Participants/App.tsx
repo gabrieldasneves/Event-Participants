6e9ba92555aa0657eb901c5523efa7b7d53b4ba7
Gabriel das Neves <galvesdasneves@gmail.com>
2023-05-07 02:18:15 -0300

    status bar style

--- Event-Participants/App.tsx
+++ Event-Participants/App.tsx
@@ -1,9 +1,14 @@
-import { View } from "react-native";
+import { View, StatusBar } from "react-native";
 import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
     <View style={{ flex: 1, padding: 24, backgroundColor: "#141416" }}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
       <Home />
     </View>
   );
